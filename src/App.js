81742509b140fb20c330e8f281ff8837559b70a8
nsharma-26 <nehasharma261994@gmail.com>
2022-07-27 15:31:16 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BottomNavigation from "./container/bottomNavigation/BottomNavigation";
+import ComponetBlock from "./container/componentBlock/ComponentBlock";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BottomNavigation />
+      <ComponetBlock />
     </div>
   );
 }
